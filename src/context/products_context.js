2fa3bd5a9fff83da6ebcd5f5fd5183433b523435
Vsevolod Slavianinov <a0974377355@gmail.com>
2022-11-14 15:05:23 +0200

    initializing

---
+++ src/context/products_context.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import React, { useContext, useEffect, useReducer } from 'react';
+import reducer from '../reducers/products_reducer';
+import { products_url as url } from '../utils/constants';
+import {
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+  GET_SINGLE_PRODUCT_BEGIN,
+  GET_SINGLE_PRODUCT_SUCCESS,
+  GET_SINGLE_PRODUCT_ERROR,
+} from '../actions';
+
+const initialState = {
+  isSidebarOpen: false,
+};
+
+const ProductsContext = React.createContext();
+
+export const ProductsProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const context = {
+    ...state,
+    openSidebar,
+    closeSidebar
+  };
+
+  function openSidebar() {
+    dispatch({ type: SIDEBAR_OPEN });
+  }
+
+  function closeSidebar() {
+    dispatch({ type: SIDEBAR_CLOSE });
+  }
+
+  return (
+    <ProductsContext.Provider value={context}>
+      {children}
+    </ProductsContext.Provider>
+  )
+};
+
+export const useProductsContext = () => {
+  return useContext(ProductsContext)
+};
