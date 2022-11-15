f2330b7f5a5a81ceca40a8b30953c456f3a7c4f8
Vsevolod Slavianinov <a0974377355@gmail.com>
2022-11-15 18:36:42 +0200

    Products page. Added grid view.

--- src/components/GridView.js
+++ src/components/GridView.js
@@ -2,8 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 import Product from './Product'
 
-const GridView = () => {
-  return <h4>Grid View</h4>
+const GridView = ({ products }) => {
+  const productsBlock = products.map((product) => {
+    return <Product key={product.id} {...product} />
+  });
+
+  return (
+    <Wrapper>
+      <div className='products-container'>
+        {productsBlock}
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
